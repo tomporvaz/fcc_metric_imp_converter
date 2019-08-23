73880841f352f8c1cce712d334d79f4fb17e426e
tomporvaz <tomporvaz@gmail.com>
2019-08-23 19:24:36 -0400

    Added helmet parent item to server.js.

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var cors        = require('cors');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
+const helmet = require('helmet.js');
 
 var app = express();
 
@@ -17,6 +18,7 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
 //Index page (static HTML)
 app.route('/')
