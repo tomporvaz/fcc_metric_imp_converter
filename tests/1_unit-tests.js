b4b87a6cf0f149dfdedb2881b24f96ff2cfda0c3
tomporvaz <tomporvaz@gmail.com>
2019-08-26 20:55:36 -0400

    Fixed spellOutUnit test.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -90,7 +90,7 @@ suite('Unit Tests', function(){
       var input = ['gal','l','mi','km','lbs','kg'];
       var expect = ['gallons', 'liters', 'miles', 'kilometers','pounds', 'kilograms'];
       input.forEach(function(ele, i) {
-        assert.equal(convertHandler.getReturnUnit(ele), expect[i]);
+        assert.equal(convertHandler.spellOutUnit(ele), expect[i]);
       });
       done();
     });
